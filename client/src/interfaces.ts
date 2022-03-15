b28d6c208fa26e343eb40a91f7e855562409ec70
Muizz Matemilola <88415295+ayokanme@users.noreply.github.com>
2022-03-14 20:16:03 -0500

    created interfaces for data returned from Top Stories API

---
+++ client/src/interfaces.ts
@@ -0,0 +1,20 @@
+export interface ArticleObject {
+  section: string,
+  subsection: string,
+  title: string,
+  abstract: string,
+  url: string,
+  byline: string,
+  published_date: string,
+  multimedia: ArticleMedia[],
+  short_url: string
+}
+
+export interface ArticleMedia {
+  url: string,
+  format: "Super Jumbo" | "threeByTwoSmallAt2X" | "Large Thumbnail",
+  height: number,
+  width: number,
+  type: string,
+  caption: string,
+}
