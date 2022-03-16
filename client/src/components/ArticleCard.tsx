420f131d0ce5dd1b5ca3d96774faa59a0b7cf3a4
Muizz Matemilola <88415295+ayokanme@users.noreply.github.com>
2022-03-16 13:29:02 -0500

    added tooltip to article link

--- client/src/components/ArticleCard.tsx
+++ client/src/components/ArticleCard.tsx
@@ -52,9 +52,11 @@ class ArticleCard extends React.Component<ArticleCardProps> {
             />
           </Tooltip>
         </Box>
-        <Link href={url} underline="hover" variant="subtitle1" gutterBottom component="span" sx={{ gridArea: 'title', margin: 'auto', fontSize: 14, cursor: 'pointer' }}>
-          {title}
-        </Link>
+        <Tooltip title="Read on NYTimes.com">
+          <Link href={url} underline="hover" variant="subtitle1" gutterBottom component="span" sx={{ gridArea: 'title', margin: 'auto', fontSize: 14, cursor: 'pointer' }}>
+            {title}
+          </Link>
+        </Tooltip>
         <Typography variant="subtitle2" gutterBottom component="div" sx={{ gridArea: 'byline', fontSize: 12 }}>
           {`${byline} (`}<TimeAgo datetime={utcTime} />{`)`}
         </Typography>
@@ -82,46 +84,3 @@ class ArticleCard extends React.Component<ArticleCardProps> {
 }
 
 export default ArticleCard;
-
-
-// return (
-//   <Card elevation={6} sx={{ display: 'block', width: 700, height: 150, margin: '10px auto', borderRadius: '10px', backgroundColor: '#f5f5f5' }}>
-//     <Box sx={{ display: 'inline-block' }}>
-//       <Tooltip title={thumbnail.caption} placement="bottom" arrow>
-//         <CardMedia
-//           component="img"
-//           sx={{ width: thumbnail.width }}
-//           image={thumbnail.url}
-//           alt={thumbnail.caption}
-//         />
-//       </Tooltip>
-//     </Box>
-//     <Box sx={{ display: 'inline-block', verticalAlign: 'top', width: 550, height: 150 }}>
-//       <CardContent sx={{ display: 'block', maxHeight: 80, padding: 0, verticalAlign: 'top' }}>
-//         <Link href={url} underline="hover" variant="subtitle1" gutterBottom component="span" sx={{ fontSize: 14, cursor: 'pointer' }}>
-//           {title}
-//         </Link>
-//         <Typography variant="subtitle2" gutterBottom component="div" sx={{ display: 'block', fontSize: 12 }}>
-//           {`${byline} (`}<TimeAgo datetime={utcTime} />{`)`}
-//         </Typography>
-//       </CardContent>
-//       <Box sx={{ display: 'block', padding: 0, maxHeight: 90, verticalAlign: 'bottom' }}>
-//         <CardContent sx={{ display: 'inline-block', width: 500, padding: 0 }}>
-//           <Typography variant="body1" gutterBottom component="div" sx={{ fontSize: 12 }}>
-//             {`${abstract}..`}
-//           </Typography>
-//           <Tooltip title={`SEE MORE ${section.toUpperCase()} NEWS`} arrow>
-//             <Button variant="outlined" sx={{ fontSize: 12, verticalAlign: 'bottom' }}>{section}</Button>
-//           </Tooltip>
-//         </CardContent>
-//         <Box sx={{ display: 'inline-block', width: 50, verticalAlign: 'bottom' }}>
-//           <Tooltip title="share article" arrow>
-//             <IconButton aria-label="share">
-//               <Share />
-//             </IconButton>
-//           </Tooltip>
-//         </Box>
-//       </Box>
-//     </Box>
-//   </Card>
-// );
