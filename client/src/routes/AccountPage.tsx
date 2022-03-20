ddc2f36d43cd6b3cc32053d57dcc8403b968f96e
Muizz Matemilola <88415295+ayokanme@users.noreply.github.com>
2022-03-20 08:56:57 -0500

    implemented logout on client side

---
+++ client/src/routes/AccountPage.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Box, Button, Divider, Stack } from '@mui/material';
+import axios from 'axios';
+import { AccountPageProps, AccountPageState } from '../interfaces';
+import AccountDeleteModal from '../components/AccountDeleteModal';
+
+class AccountPage extends React.Component<AccountPageProps, AccountPageState>{
+  constructor(props: AccountPageProps) {
+    super(props);
+    this.logout = this.logout.bind(this);
+  }
+
+  logout () {
+    axios.get('/auth/logout')
+      .then(() => {
+        this.props.verifyUser();
+      })
+  }
+
+  modalHandler () {
+    //do next
+  }
+
+  render() {
+    return (
+      <div className="Account" style={{ textAlign: 'center', height: '100%' }}>
+        <Box sx={{
+          position: 'absolute' as 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          height: '40%',
+          width: 500
+        }}>
+          <Stack direction="column" spacing={2} alignItems="center" justifyContent="center">
+            <Button onClick={this.logout} variant="contained">LOGOUT</Button>
+            <Divider />
+            <Button onClick={this.modalHandler} variant="outlined" color="error">DELETE ACCOUNT</Button>
+          </Stack>
+        </Box>
+      </div>
+    );
+  }
+}
+
+export default AccountPage;
